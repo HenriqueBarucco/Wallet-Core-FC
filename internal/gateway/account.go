ed759acc04e0107ca80de4811c55843beddda0c3
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-03 20:14:28 -0300

    feat: add create account usecase

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/HenriqueBarucco/Wallet-Core-FC/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	FindByID(id string) (*entity.Account, error)
+}
