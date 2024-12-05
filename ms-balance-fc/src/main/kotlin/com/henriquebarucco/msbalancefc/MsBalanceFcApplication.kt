e322357e28ee7cae90620779d3154aba0f00753b
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-05 19:51:37 -0300

    feat: initial ms balance commit

---
+++ ms-balance-fc/src/main/kotlin/com/henriquebarucco/msbalancefc/MsBalanceFcApplication.kt
@@ -0,0 +1,11 @@
+package com.henriquebarucco.msbalancefc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsBalanceFcApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsBalanceFcApplication>(*args)
+}
---
+++ ms-balance-fc/src/test/kotlin/com/henriquebarucco/msbalancefc/MsBalanceFcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.henriquebarucco.msbalancefc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsBalanceFcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
